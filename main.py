1a8ccf8d4f60ca1e216f58761520fc1bdcab2352
HenriqueUrbano <henriqueurbano21@gmail.com>
2022-02-20 14:21:25 -0300

    Correção no cálculo de lucro/prejuízo

--- main.py
+++ main.py
@@ -15,17 +15,28 @@ symbols = ["SHIBBRL",
            "BNBBRL",
            "LTCBRL"]
 
-avgprices = [0.00012,1.034,332917,23096.90,13.7241,5.313,240.81,0.0001727,3599,1283.9]
+avgprices = [0.00012,
+            1.034,
+            332917,
+            23096.90,
+            13.7241,
+            5.313,
+            240.81,
+            0.0001727,
+            3599,
+            1283.9]
+
 profit = ""
 counter = 0
 for i in symbols:
     x = urllib.request.urlopen(api + "?symbol=" + i).read()
     json_data = json.loads(x)
-    result = 100 - ((avgprices[counter] * 100) / float(json_data['price']))
-    if result > 0:
-        profit = "Lucro de " + str(result) + "% \n"
+    result = float(json_data['price']) - avgprices[counter]
+    percentage = (100 * result) / avgprices[counter]
+    if percentage > 0:
+        profit = "Lucro de " + str(percentage) + "% \n"
     else:
-        profit = "Prejuízo de " + str(result) + "% \n"
+        profit = "Prejuízo de " + str(percentage) + "% \n"
     print(json_data['symbol'] + ' Preço atual: ' + json_data['price'])
     print("Preço Médio pago: " + str(avgprices[counter]))
     print("Resultado = " + profit)
