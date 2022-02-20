1cbd41af6d8f1860449ec3d68149c74810221fb9
HenriqueUrbano <henriqueurbano21@gmail.com>
2022-02-20 08:57:57 -0300

    Inclusão de aviso de Lucro ou Prejuízo

--- main.py
+++ main.py
@@ -15,8 +15,17 @@ symbols = ["SHIBBRL",
            "BNBBRL",
            "LTCBRL"]
 
-prices = []
-
+avgprices = [0.00012,1.034,332917,23096.90,13.7241,5.313,240.81,0.0001727,3599,1283.9]
+profit = ""
+counter = 0
 for i in symbols:
     x = urllib.request.urlopen(api + "?symbol=" + i).read()
-    print(x)
+    json_data = json.loads(x)
+    result = float(json_data['price']) - avgprices[counter]
+    if result > 0:
+        profit = "Lucro!"
+    else:
+        profit = "Prejuízo!"
+    print(json_data['symbol'] + ' Price: ' + json_data['price'])
+    print("Resultado = " + profit)
+    counter = counter + 1
